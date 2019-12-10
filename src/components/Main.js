f3d7b8b2c66b7d4b7018cc40154de1ab3df58fa5
NickHimmel <himmel.nick@gmail.com>
2019-12-10 13:37:08 -0500

    Add Button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Button from './Button.js'
 
 class Main extends Component {
 
   render() {
     return (
       <div>
-        <p>App For Testing</p>
+        <p>This is an app For testing</p>
+        <Button />
       </div>
     );
   }

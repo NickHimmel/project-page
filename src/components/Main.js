92db964fe9eed350ca1bb8c0da29c88bd2f5c54b
NickHimmel <himmel.nick@gmail.com>
2019-12-10 13:15:29 -0500

    change second h1 to p tag

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>App For Testing</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
